c54f7bd1f2a837f213695dcc96c421c70a08056e
Lamido Tijjani <kinglamido@gmail.com>
2018-07-03 00:36:50 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -4,14 +4,19 @@ class Dog
     @breed = breed
   end
   
-  def name
-    @name
-    @breed
+  def name=(new_name)
+    @name = new_name
   end
   
-  def name=(new_name, new_breed)
-    new_name, new_breed = name_breed.split()
-    @name = new_name
+    def name
+    @name
+  end
+  
+  def breed=(new_breed)
     @breed = new_breed
+  end
+  
+    def breed
+    @breed
   end
 end
